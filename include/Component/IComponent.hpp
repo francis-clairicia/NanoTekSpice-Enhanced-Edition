c1425e2c7cd330906003c3fa650a0ff12e702c61
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-05-17 20:48:51 +0200

    [UPDATE] Nothing notifiable

--- include/Component/IComponent.hpp
+++ include/Component/IComponent.hpp
@@ -20,12 +20,12 @@ namespace nts
 
     class IComponent
     {
-        public:
-            virtual ~IComponent() noexcept = default;
-            virtual void simulate(std::size_t tick) = 0;
-            virtual nts::Tristate compute(std::size_t pin) = 0;
-            virtual void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
-            virtual void dump() const noexcept = 0;
+    public:
+        virtual ~IComponent() noexcept = default;
+        virtual void simulate(std::size_t tick) = 0;
+        virtual nts::Tristate compute(std::size_t pin) = 0;
+        virtual void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
+        virtual void dump() const noexcept = 0;
     };
 }
 
